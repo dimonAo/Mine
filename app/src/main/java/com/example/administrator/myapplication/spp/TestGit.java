688fa52aae3489b85e2ddaedc7c1c606554920d4
dimonAO <dimon_ao@foxmail.com>
2018-01-17 23:06:18 +0800

    Test Android Studio Git

---
+++ app/src/main/java/com/example/administrator/myapplication/spp/TestGit.java
@@ -0,0 +1,13 @@
+package com.example.administrator.myapplication.spp;
+
+/**
+ * =================================
+ * 作    者：dimonao
+ * 版    本：1.0
+ * 创建日期：2018/1/17 23:05
+ * 描    述：
+ * 修订历史：
+ * =================================
+ */
+public class TestGit {
+}
